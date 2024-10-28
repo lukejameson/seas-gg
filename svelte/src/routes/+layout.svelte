53540a18da74a71a6baee38a61df3e12affa7b6f
lukejamesonboolooloo <luke.j@boolooloo.network>
2024-10-28 15:55:22 +0000

    styling and layout

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script>
-    /** @type {import('./$types').LayoutData} */
-    export let data;
+	/** @type {import('./$types').LayoutData} */
+	export let data;
 
-    import "../app.css"
+	import '../app.css';
+	import './layout.css';
 </script>
 
-<slot></slot>
+<div class="body">
+	<div class="side-container">
+
+	</div>
+	<div class="header">
+		<div class="title-card">
+			<h1>Tides</h1>
+		</div>
+	</div>
+	<div class="sub-content">
+		<!-- <slot></slot> -->
+	</div>
+</div>
