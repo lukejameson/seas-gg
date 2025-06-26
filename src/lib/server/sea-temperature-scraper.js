a4c66bc8b80a277cf7fe87895c76f0a744127900
CrazedDog <lukejameson@live.co.uk>
2025-06-26 09:22:43 +0100

    fix chrouim path

--- src/lib/server/sea-temperature-scraper.js
+++ src/lib/server/sea-temperature-scraper.js
@@ -17,6 +17,7 @@ class SeaTemperatureScraper {
 		try {
 			const browser = await chromium.launch({
 				headless: true,
+				executablePath: process.env.CHROMIUM_PATH || '/usr/bin/chromium',
 				args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
 			});
 
